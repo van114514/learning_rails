133a62160b6fbd8f27692999a06cf518bdbd06a8
van114514 <van45450721919@gmail.com>
2020-03-28 16:50:18 +0900

    Finish user signup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,8 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  #add "with: :threads" for test
+  parallelize(workers: :number_of_processors,with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
