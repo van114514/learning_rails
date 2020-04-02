041d86fdc192bcc304ed319ac29be6bd508820ed
van114514 <van45450721919@gmail.com>
2020-04-02 19:57:29 +0900

    Finish user edit, update, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+  end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
